4c25707f9c8f0a6638102133973ba7681c3a43f9
argonavt11 <argonavt.al@yandex.ru>
2018-09-27 11:05:22 +0300

    Was added timestamp points on line of progress

--- src/App.js
+++ src/App.js
@@ -2,79 +2,22 @@ import React, { Component } from 'react';
 import "./App.css"
 import LeafletReactTrackPlayer from "components/laeflet-react-track-player"
 import L from "leaflet"
+import demo from "./demo";
 import { Map, TileLayer } from "react-leaflet"
 
 class App extends Component {
   state = {
     lat: 47.445745,
     lng: 40.272891666666666,
-    zoom: 15,
+    zoom: 13,
   }
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position = [demo[0].lat, demo[0].lng]
     return (
       <div className="App">
         <Map center={position} zoom={this.state.zoom}>
           <LeafletReactTrackPlayer
-            track={
-              [{
-                LOAD: 40.88,
-                SPEED: 4.4,
-                course: 0,
-                lat: 47.445745,
-                lng: 40.272891666666666,
-                status: 1,
-                t: "180921100658000",
-              }, {
-                LOAD: 27.74,
-                SPEED: 0.88,
-                course: 328,
-                lat: 47.443255,
-                lng: 40.273045,
-                status: 1,
-                t: "180921100947000"
-              }, {
-                LOAD: 50.75,
-                SPEED: 12.77,
-                course: 258,
-                lat: 47.443255,
-                lng: 40.2730457,
-                status: 1,
-                t: "180921100958000"
-              }, {
-                LOAD: 50.75,
-                SPEED: 12.77,
-                course: 258,
-                lat: 47.44325166666667,
-                lng: 40.27352,
-                status: 4,
-                t: "180921101043000"
-              }, {
-                LOAD: 50.75,
-                SPEED: 12.77,
-                course: 258,
-                lat: 47.4432433333333357,
-                lng: 40.274631666666664,
-                status: 4,
-                t: "180921101128000"
-              }, {
-                LOAD: 50.75,
-                SPEED: 12.77,
-                course: 258,
-                lat: 47.44323333333333,
-                lng: 40.276125,
-                status: 4,
-                t: "180921101228000"
-              }, {
-                LOAD: 50.75,
-                SPEED: 12.77,
-                course: 258,
-                lat: 47.44323166666667,
-                lng: 40.277606666666664,
-                status: 4,
-                t: "180921101328000"
-              }]
-            }
+            track={demo}
             useMultyPolyline={true}
             optionMultyIdxFn={function (p) {
               return p.status;
